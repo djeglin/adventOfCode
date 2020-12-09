12e45c1d547543af2bc08ab21122a5e68e23da42
djeglin <djeglin@gmail.com>
2020-12-09 14:56:38 +0000

    complete day 3

---
+++ 2020/day3/solution.js
@@ -0,0 +1,42 @@
+export default class Solution {
+  constructor(data) {
+    this.data = data.split('\n').map((row) => row.trim().split(''))
+  }
+
+  start() {
+    const movements = [
+      [1, 1],
+      [3, 1],
+      [5, 1],
+      [7, 1],
+      [1, 2],
+    ]
+    const trees = movements.map((movement) => this.traverse(movement))
+    console.log(trees)
+    return this.getProduct(trees)
+  }
+
+  traverse(movement) {
+    const [right, down] = movement
+    let row = 0
+    let col = 0
+    let trees = 0
+    while (row < this.data.length - 1) {
+      console.log('row:', row, 'col:', col, this.data[row])
+      if (row + down >= this.data.length - 1) break
+      if (col + right >= this.data[row].length) {
+        col = right - (this.data[row].length - col)
+      } else {
+        col = col + right
+      }
+      row = row + down
+      const space = this.data[row][col]
+      if (space === '#') trees++
+    }
+    return trees
+  }
+
+  getProduct(nums) {
+    return nums.reduce((a, b) => a * b)
+  }
+}
